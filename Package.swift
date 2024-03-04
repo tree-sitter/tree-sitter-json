e1c6301c6a1dbd2ad0d168fd8b0ab35789ba4715
Amaan Qureshi <amaanq12@gmail.com>
2024-03-03 21:53:25 -0500

    build: update bindings and versions

--- Package.swift
+++ Package.swift
@@ -1,10 +1,8 @@
 // swift-tools-version:5.3
-
 import PackageDescription
 
 let package = Package(
     name: "TreeSitterJSON",
-    platforms: [.macOS(.v10_13), .iOS(.v11)],
     products: [
         .library(name: "TreeSitterJSON", targets: ["TreeSitterJSON"]),
     ],
@@ -13,16 +11,27 @@ let package = Package(
         .target(name: "TreeSitterJSON",
                 path: ".",
                 exclude: [
-                    "binding.gyp",
-                    "bindings",
                     "Cargo.toml",
-                    "corpus",
+                    "Makefile",
+                    "binding.gyp",
+                    "bindings/c",
+                    "bindings/go",
+                    "bindings/node",
+                    "bindings/python",
+                    "bindings/rust",
+                    "prebuilds",
                     "grammar.js",
-                    "LICENSE",
                     "package.json",
-                    "README.md",
-                    "src/grammar.json",
-                    "src/node-types.json",
+                    "package-lock.json",
+                    "pyproject.toml",
+                    "setup.py",
+                    "test",
+                    "examples",
+                    ".editorconfig",
+                    ".github",
+                    ".gitignore",
+                    ".gitattributes",
+                    ".gitmodules",
                 ],
                 sources: [
                     "src/parser.c",
@@ -32,5 +41,6 @@ let package = Package(
                 ],
                 publicHeadersPath: "bindings/swift",
                 cSettings: [.headerSearchPath("src")])
-    ]
+    ],
+    cLanguageStandard: .c11
 )
--- bindings/rust/build.rs
+++ bindings/rust/build.rs
@@ -1,10 +1,12 @@
 fn main() {
-    cc::Build::new()
-        .include("src")
-        .flag_if_supported("-Wno-unused-parameter")
-        .flag_if_supported("-Wno-unused-but-set-variable")
-        .flag_if_supported("-Wno-trigraphs")
-        .file("src/parser.c")
-        .compile("parser");
-    println!("cargo:rerun-if-changed=src/parser.c");
+    let src_dir = std::path::Path::new("src");
+
+    let mut c_config = cc::Build::new();
+    c_config.std("c11").include(src_dir);
+
+    let parser_path = src_dir.join("parser.c");
+    c_config.file(&parser_path);
+    println!("cargo:rerun-if-changed={}", parser_path.to_str().unwrap());
+
+    c_config.compile("tree-sitter-json");
 }
